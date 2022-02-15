6e3c4c94c8020bb6606053b5fc0d2e3eecb68bf9
jayrathod26 <jkrathod2601@gmail.com>
2022-02-15 16:36:44 +0530

    Add_req_info

--- simple_server_practice/http_server.js
+++ simple_server_practice/http_server.js
@@ -1,12 +1,27 @@
 const http=require('http');
 
-const server = http.createServer((req,res) => {
-    console.log(req.headers.host)
-    console.log(req.headers.userAgent)
+const server = http.createServer(async(req,res) => {
+    
+    // console.log(req.headers.host)
+    // console.log(req.headers.userAgent)
+    console.log(req.rawHeaders)
+    console.log(req.url)
+    res.writeHead(200, { "content-type": "application/json" });
     res.write("hello world")
-    res.end()
+    await setTimeout(()=>{
+        server.close(()=>{
+            console.log("server closed")
+        })
+    },3000)
+    // process.exit()
 })
 
 server.listen(3000,()=>{
     console.log('server started')
 })
+
+// setTimeout(()=>{
+//     server.close(()=>{
+//         console.log("server closed")
+//     })
+// },3000)
