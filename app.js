0f82af4be982f10c1c4545fd4b03ace3fcdfa0be
jayrathod26 <jkrathod2601@gmail.com>
2022-02-17 12:14:03 +0530

    Add

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app=express()
 const userrout=require('./routes/user')
 
 app.use(userrout)
+// add prefix at the bigning
+//app.use("/admin", userrout)
+// it access by /admin/add ex
 
 // app.use((req,res,next) => {
 //     console.log("hello from 1st")
@@ -14,8 +17,13 @@ app.use(userrout)
 //     res.send("<h1>hello from server 2</h1>")
 // })
 
-app.get("*",(req,res)=>{
-    res.send("error found plz check some other page")
+// page not found
+// app.get("*",(req,res)=>{
+//     res.send("error found plz check some other page")
+// })
+//page  not found
+app.use((req,res,next)=>{
+    res.status(404).send("not found")
 })
 
 app.listen(3000,()=>{
