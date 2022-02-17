e80038959d3d445365d8b1a955388c1519ac00b2
jayrathod26 <jkrathod2601@gmail.com>
2022-02-17 16:37:35 +0530

    Add Nodemailer

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,8 @@ const path=require('path')
 
 const product_data=[]
 
+var nodemailer = require('nodemailer');
+const { route } = require('express/lib/router')
 // set ejs
 
 router.get('/',(req, res)=>{
@@ -30,6 +32,34 @@ router.post('/addproduct',(req,res)=>{
     product_data.push(req.body.product_name)
     // res.sendFile(path.join(__dirname, '../','views','add_product.html'))
     res.redirect('/')
+})
+
+router.get('/sendmail',(req,res)=>{
+    res.render('mail')
+})
+
+router.post('/sendmail',async(req,res)=>{
+    // console.log(req.body.email)
+    let transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+           user:'jkrathod2601@gmail.com',
+           pass:'RJ@$BORN@$2601'
+        }
+    });
+    const message = {
+        from: 'jkrathod2601@gmail.com', // Sender address
+        to: req.body.email,         // List of recipients
+        subject: "testing nodemailer", // Subject line
+        html:'<h3>You successfully send the mail</h3><h1>'// Plain text body
+    };
+    await transport.sendMail(message, function(err, info) {
+        if (err) {
+          res.send(err)
+        } else {
+            res.redirect('/')
+        }
+    });
 })
 
 module.exports=router
