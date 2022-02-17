2f390de614b60b913894293739535e5bfe170de6
jayrathod26 <jkrathod2601@gmail.com>
2022-02-17 15:16:16 +0530

    Ejs Add

--- app.js
+++ app.js
@@ -3,13 +3,21 @@ const app=express()
 const path=require('path');
 const userrout=require('./routes/user')
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(userrout)
 
+// setting ejs
+app.set('view engine', 'ejs')
+
+const x=[1,2,3]
 
 
 app.get("*",(req,res)=>{
-    res.status(404).sendFile(path.join(__dirname, './','views','404.html'))
+    // res.status(404).sendFile(path.join(__dirname, './','views','404.html'))
+    res.render('404')
 })
 
 app.listen(3000,()=>{
--- routes/user.js
+++ routes/user.js
@@ -2,20 +2,32 @@ const express=require('express')
 const app=express()
 const router=express.Router()
 const path=require('path')
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+
+const product_data=[]
+
+// set ejs
 
 router.get('/',(req, res)=>{
     console.log("routing is calling and it is simple routing")
-    res.sendFile(path.join(__dirname, '../','views','shop.html'))
+    res.render('shop',{
+        about:"this is page about to learn EJS",
+        data:product_data
+    })
 })
 
+// without view engine
+// router.get('/addproduct',(req,res)=>{
+//     res.sendFile(path.join(__dirname, '../','views','add_product.html'))
+// })
+
+
 router.get('/addproduct',(req,res)=>{
-    res.sendFile(path.join(__dirname, '../','views','add_product.html'))
+    res.render('add_product')
 })
 
 router.post('/addproduct',(req,res)=>{
     console.log(req.body)
+    product_data.push(req.body.product_name)
     // res.sendFile(path.join(__dirname, '../','views','add_product.html'))
     res.redirect('/')
 })
