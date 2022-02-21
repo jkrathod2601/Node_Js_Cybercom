ccbaa991f6ca284573683a8a9ec22552e2e399c4
jayrathod26 <jkrathod2601@gmail.com>
2022-02-21 20:03:00 +0530

    Add .env file and Complate dynamic routing

--- core/route.js
+++ core/route.js
@@ -1,24 +1,51 @@
-const data=require('./route.json')
-const express=require('express')
+const data = require("./route.json");
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+const router = new express.Router();
 
-const app=express()
-const router=new express.Router()
-const home=require('../controller/home')
-const product=require('../controller/product')
-const auth=require('../core/auth.js')
-
-
-
+// accessing the all file name of middleware and controller
+const file_name_controller = fs.readdirSync(
+  path.join(__dirname, "../controller")
+);
+const file_name_middleware = fs.readdirSync(
+  path.join(__dirname, "../middleware")
+);
+// creating the object that imports validate file
+const imports = {};
+const imports_controller = {};
+// console.log(file_name)
 // console.log('from router file')
-console.log('--------------------------------')
-
-
-data.forEach((ele)=>{
-    let middlewares=ele.middlewares.map((x)=>{
-        return eval(x)
-    })
-    router[ele.method](ele.path,[...middlewares],eval(ele.controller))
-})
-
-module.exports=router
-console.log('--------------------------------')
+console.log("--------------------------------");
+data.forEach((ele) => {
+  try {
+    // for middleware require
+    let middlewares = ele.middlewares.map((x) => {
+      // get file name
+      let value = x.split(".")[0];
+      // get middleware name
+      let value2 = x.split(".")[1];
+      // require this into tyhe middleware object
+      imports[value] = require(`../middleware/${value}`);
+      // console.log(imports[value][value2])
+      return imports[value][value2];
+    });
+    //imort controller
+    imports_controller[ele.controller.split(".")[0]] = require(`../controller/${
+      ele.controller.split(".")[0]
+    }`);
+    const controller = eval(
+      imports_controller[ele.controller.split(".")[0]][
+        ele.controller.split(".")[1]
+      ]
+    );
+    // creating the dynamic route
+    router[ele.method](ele.path, middlewares, controller);
+  } catch (error) {
+    console.log("check your controller or middlewares path and name");
+  }
+});
+// console.log(imports)
+module.exports = router;
+console.log("--------------------------------");
--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express=require('express');
 const app= express();
 const router=require("../core/route.js")
 
+require('dotenv').config()
+const port=process.env.port || 8080
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 require("../util/databse")
@@ -12,6 +15,7 @@ app.use(router)
 
 
 
-app.listen(3000,()=>{
-    console.log("server started on port 3000");
+app.listen(port, ()=>{
+    console.log(process.env.port)
+    console.log(`server started on port ${port}`);
 })
