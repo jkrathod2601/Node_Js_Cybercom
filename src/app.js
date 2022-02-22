aa42d10f7b28390da6d62f786938db952883c84b
jayrathod26 <jkrathod2601@gmail.com>
2022-02-22 18:17:16 +0530

    Add dynamic port

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
-require('dotenv').config()
-const express=require('express');
-const app= express();
-const router=require("../core/route.js")
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const router = require("../core/route.js");
 // const chalk=require('chalk')
-const colors = require('colors');
-const sequelize=require("../util/databse")
-const portcheck=require("../src/extra/portcheck")
-let port=process.env.port || 8080
+const colors = require("colors");
+const sequelize = require("../util/databse");
+const portcheck = require("../src/extra/portcheck");
+let port = process.env.port || 8080;
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const readline = require('readline').createInterface({
+const readline = require("readline").createInterface({
   input: process.stdin,
-  output: process.stdout
-})
+  output: process.stdout,
+});
 // require("../util/databse")
 // console.log(router)
 
@@ -25,43 +25,62 @@ const readline = require('readline').createInterface({
 // define model
 // const User=require("../model/user.js")
 
+app.use(router);
 
-app.use(router)
-
-const servercreation=async()=>{
-  await portcheck(port).then((data)=>{
-    if(data==true){
-      app.listen(port, () => {
-        console.log(colors.green.underline(`server started on port ${port}`));
-        return
-      });
-    }else{
-      console.log(colors.red(`${port} is allready listening`))
-      port=parseInt(port)+1
-      console.log(`are you ready to start this with port number ${port} yes/no?`)
-      readline.question(`yes or no?`, answer_port_choice => {
-        if(answer_port_choice=="yes"){
-          servercreation()
-        }else{
-          return
+const servercreation = async () => {
+  await portcheck(port).then((data) => {
+    if (data == false) {
+      port = eval(port) + 1;
+      servercreation();
+    } else {
+      readline.question(
+        `yes or no for creating server on port ${port}?`,
+        (answer_port_choice) => {
+          if (answer_port_choice == "yes") {
+            app.listen(port, () => {
+              console.log(
+                colors.green.underline(`server started on port ${port}`)
+              );
+              return;
+            });
+          } else {
+            return;
+          }
         }
-        // readline.close()
-      })
+      );
     }
-  })
-}
-servercreation()
- 
+  });
+};
 
-
-
-
+// const servercreation=async()=>{
+//   await portcheck(port).then((data)=>{
+//     if(data==true){
+//       app.listen(port, () => {
+//         console.log(colors.green.underline(`server started on port ${port}`));
+//         return
+//       });
+//     }else{
+//       console.log(colors.red(`${port} is allready listening`))
+//       port=parseInt(port)+1
+//       console.log(`are you ready to start this with port number ${port} yes/no?`)
+//       readline.question(`yes or no?`, answer_port_choice => {
+//         if(answer_port_choice=="yes"){
+//           servercreation()
+//         }else{
+//           return
+//         }
+//         // readline.close()
+//       })
+//     }
+//   })
+// }
+servercreation();
 
 // sequelize
 //   .sync()
 //   .then((data) => {
 //     // console.log(data);
-    
+
 //   })
 //   .catch((err) => {
 //     console.log(err);
