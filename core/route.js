cc9909bc5d1e2f3e02331edde40840786aee2a01
jayrathod26 <jkrathod2601@gmail.com>
2022-02-22 17:13:47 +0530

    Add error handling of route.json file

--- core/route.js
+++ core/route.js
@@ -2,28 +2,42 @@ const route_json_array = require("../util/route.json");
 const express = require("express");
 const router = new express.Router();
 const validateerror=require('../src/extra/validateerror')
+const colors=require('colors')
 
 
-
-route_json_array.forEach((ele,index) => {
-  try {
-    // setting up middleware
-    let middlewares = ele.middlewares.map((x) => {
-      let middleware_file_name = x.split(".")[0];
-      let middleware_function_name = x.split(".")[1];
-      const middleware_file = require(`${process.env.middleware_path}${middleware_file_name}`);
-      return middleware_file[middleware_function_name];
+try {
+  if(Array.isArray(route_json_array)){
+    route_json_array.forEach((ele,index) => {
+      try {
+        // setting up middleware
+        let middlewares = ele.middlewares.map((x) => {
+          let middleware_file_name = x.split(".")[0];
+          let middleware_function_name = x.split(".")[1];
+          const middleware_file = require(`${process.env.middleware_path}${middleware_file_name}`);
+          return middleware_file[middleware_function_name];
+        });
+        //setting up controller
+        let [controller_filname, controller_functionname] =ele.controller.split(".");
+        const controller_calling = require(`${process.env.controller_path}${controller_filname}`);
+        const controller = controller_calling[controller_functionname];
+    
+        router[ele.method](ele.path, middlewares, controller);
+      } catch (error) {
+        validateerror(ele,index)
+      }
     });
-    //setting up controller
-    let [controller_filname, controller_functionname] =ele.controller.split(".");
-    const controller_calling = require(`${process.env.controller_path}${controller_filname}`);
-    const controller = controller_calling[controller_functionname];
-
-    router[ele.method](ele.path, middlewares, controller);
-  } catch (error) {
-    validateerror(ele,index)
+  }else{
+    throw new Error("enter the correct format of route.json file")
   }
-});
+} catch (error) {
+  console.log(colors.red(error.message))
+}
+
+
+  
+
+
+
 
 module.exports = router;
 
