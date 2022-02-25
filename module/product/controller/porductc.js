a26e82bebbf9e214391aba394a008c18db71e80e
jayrathod26 <jkrathod2601@gmail.com>
2022-02-25 18:34:19 +0530

    Add Function

--- module/product/controller/porductc.js
+++ module/product/controller/porductc.js
@@ -1,3 +1,4 @@
 module.exports.done=(req,res)=>{
+    framework.product.function.pfunction.add()
     console.log(framework.chalk.yellow("productc.js is calling controller"))
 }
--- module/product/controller/productcontroller.js
+++ module/product/controller/productcontroller.js
@@ -1,3 +1,4 @@
 module.exports.done1=(req,res)=>{
+    framework.product.service.connect.connect()
     console.log(framework.chalk.yellow("productc.js is calling controller"))
 }
