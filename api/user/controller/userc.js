d810222fa56751967091b9f966bebda30d69f8ef
jayrathod26 <jkrathod2601@gmail.com>
2022-03-16 16:58:25 +0530

    Add

--- api/user/controller/userc.js
+++ api/user/controller/userc.js
@@ -62,21 +62,19 @@ module.exports.refreshtoken=async(req,res)=>{
 // with data base
 module.exports.login = async(req,res)=>{
     try{
-       let refreshtoken_user=jwt.sign({username:req.body.name},req.cookies["_csrf"],{expiresIn: "2h"})
+       let refreshtoken_user=jwt.sign({username:req.body.username},req.cookies["_csrf"],{expiresIn: "2h"})
        await db.user.create({
             name:req.body.username,
             role:req.body.role,
             refreshtoken:refreshtoken_user,
             key:crypto.randomBytes(16).toString('hex')
         }).then((data)=>{
-            console.log()
             let accesstoken_user=jwt.sign({username:req.body.name,role:req.body.role,uid:data.dataValues.id},framework.jwtkey,{expiresIn: "1h"})
             res.cookie("refresh_token",refreshtoken_user,{ maxAge:3650*24*60*60, httpOnly: true ,signed: true})
             res.cookie("access_token",accesstoken_user,{ maxAge: 3650*24*60*60, httpOnly: true ,signed: true})
             res.render('index',{title: req.body.username})
             console.log(framework.chalk.green("useradded successfully"))
         })
-
     }catch(e){
         console.log(framework.chalk.red(e))
     }
@@ -84,15 +82,19 @@ module.exports.login = async(req,res)=>{
 
 
 module.exports.refreshdatabase=async(req,res)=>{
-    let key_id=req.params.id
+    try{
+        let key_id=req.params.id
     await db.user.findAll({where:{key:key_id},raw:true}).then((data)=>{
         if(data.length==1){
             // console.log(data[0].refreshtoken)
             // console.log(req.signedCookies["refresh_token"])
             if(data[0].refreshtoken==req.signedCookies["refresh_token"]){
                 const data_token=jwt.verify(req.signedCookies["refresh_token"],req.cookies["_csrf"])
-                let access_token=jwt.sign({role:data.role,uid:data.id,username:data.name},framework.jwtkey,{expiresIn: "1h"})
-                let refresh_token=jwt.sign({uid:data.id},req.cookies["_csrf"],{expiresIn: "2h"})
+                console.log(data_token)
+                let access_token=jwt.sign({role:data[0].role,uid:data[0].id,username:data[0].name},framework.jwtkey,{expiresIn: "1h"})
+                console.log(access_token)
+                let refresh_token=jwt.sign({uid:data[0].id},req.cookies["_csrf"],{expiresIn: "2h"})
+                console.log(refresh_token)
                 db.user.update({refreshtoken:refresh_token,key:crypto.randomBytes(16).toString('hex')},{where:{key:key_id}}).
                 then((data)=>{
                     res.cookie("refresh_token",refresh_token,{ maxAge:3650*24*60*60, httpOnly: true ,signed: true})
@@ -106,6 +108,11 @@ module.exports.refreshdatabase=async(req,res)=>{
             res.end("access denied");
         }
     })
+    }
+    catch(err){
+        console.log(framework.chalk.red(err))
+    }
+    
 }
 
 
