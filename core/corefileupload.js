3f95bf7ec95632335544908abc7cd386e87bc535
jayrathod26 <jkrathod2601@gmail.com>
2022-03-31 11:14:44 +0530

    Add

--- core/corefileupload.js
+++ core/corefileupload.js
@@ -3,26 +3,26 @@ const router = new express.Router()
 const multer=require('multer')
 const path=require('path')
 
-const filestorage=multer.diskStorage({
-    destination:(req,file,cb)=>{
-      req.filesubmission=true
-      cb(null,path.join(__dirname,`../public/${filename}`))
-    },
-    filename:(req,file,cb)=>{
-      cb(null,file.fieldname+'-'+Date.now() + '-' + Math.round(Math.random() * 1E9)+file.originalname)
-    }
-})
+// const filestorage=multer.diskStorage({
+//     destination:(req,file,cb)=>{
+//       req.filesubmission=true
+//       cb(null,path.join(__dirname,`../public/${filename}`))
+//     },
+//     filename:(req,file,cb)=>{
+//       cb(null,file.fieldname+'-'+Date.now() + '-' + Math.round(Math.random() * 1E9)+file.originalname)
+//     }
+// })
 
-const filefilter=(req,file,cb)=>{
-  req.filesubmission=true
-  if(allow_file_upload_array.includes(file.mimetype)){
-    cb(null,true)
-  }else{
-    cb(null,false)
-  }
-}
+// const filefilter=(req,file,cb)=>{
+//   req.filesubmission=true
+//   if(allow_file_upload_array.includes(file.mimetype)){
+//     cb(null,true)
+//   }else{
+//     cb(null,false)
+//   }
+// }
 
-router.use(multer({storage:filestorage,fileFilter:filefilter}).single('file'))
+// router.use(multer({storage:filestorage,fileFilter:filefilter}).single('file'))
 
 
 
